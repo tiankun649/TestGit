a12a0ef636a790bbb5ef8c2fcb242f71f757b738
tom_glb <goodMorning_glb@taku.com>
2020-05-08 17:42:36 +0800

    commit hot_fix3

--- src/main/java/cn/taku/Happy.java
+++ src/main/java/cn/taku/Happy.java
@@ -5,7 +5,7 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("right.....");
 		System.out.println("left.....");
-		System.out.println("分支hot fix...");
+		System.out.println("分支hot fix...222");
 	}
 
 }
